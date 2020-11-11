3e83a9c1d56a6a39f5cb8488b1bf986cf371a80c
Adam Dachenhausen <ad10dach@siena.edu>
2020-11-11 16:06:00 -0500

    Added platters to HDD

--- HDD.java
+++ HDD.java
@@ -1,12 +1,33 @@
-/**
+/*
   This is a simulation of a Hard Drive Disk
  */
 class HDD{
-    int space;
-    process[] HDDProcessArray;
-    public HDD(){
-        this.space = 1000;
+    //The overall size of this
+    int size;
+
+    //The spaceLeft on this
+    int spaceLeft;
+
+    //The number of platters that equally divide the size
+    int numPlatters;
+
+    //Our array of platters
+    platter[] platters;
+    /**
+       Sets up a hard drive to be added to the system.
+
+       @param size, the size in bytes of this
+       @param numPlatters, the number of single sided platters that this contains.
+     **/
+    public void initializeHDD(int size, int numPlatters){
+	this.size=size;
+	this.numPlatters=numPlatters;
+
+	// Now we are going to make the virtual hard drive
+	platters = new platter[numPlatters];
+	
+	for(int i=0; i<numPlatters; i++){
+	    platters[i] = new platter(size/numPlatters);
+	}
     }
-    
-    
 }
