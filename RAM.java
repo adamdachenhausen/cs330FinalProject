87d3a91bbba79e9ad3ffda03ec20b81a0b3a67cd
Adam Dachenhausen <ad10dach@siena.edu>
2020-11-11 16:50:01 -0500

    Added byte array

--- RAM.java
+++ RAM.java
@@ -2,7 +2,13 @@
   This is a simulation of Main System Memory aka Random Access Memory
  */
 class RAM{
-    public RAM(){
-        
+    //The size in bytes of this
+    int size;
+    //The array to hold our data
+    byte[] data;
+    
+    public RAM(int size){
+        this.size = size;
+	data = new byte[size];
     }
 }
