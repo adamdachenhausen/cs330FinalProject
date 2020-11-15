8ffa36b20debe6e567471d1b20fe4128a5e1781b
Adam Dachenhausen <ad10dach@siena.edu>
2020-11-15 18:18:47 -0500

    Added read and write method

--- CPU.java
+++ CPU.java
@@ -7,13 +7,37 @@ class CPU extends Thread{
     int cores;
 
     //The first cache in this
-    byte[] cache1;
+    private byte[] cache1;
 
     //THe size of the first cache
     int cache1Size;
+    /**
+       Constructor for the CPU class
+       @param numCores the number of cores that this should have (currently not supported)
+       @param cacheSize the size of the cache of this
+     **/
     public CPU(int numCores, int cacheSize){
      cores = numCores;
      cache1Size = cacheSize;
+    }
+    /**
+       Reads the data in this' cache
+       (Essentially just returns cache1)
+       there should be no delay as this is stored on RAM, and the cache
+       in a real system will be much faster
+     **/
+    public byte[] read(){
+	return cache1
+    }
+    /**
+       Writes input into the cache.
+       If input is bigger, then we will recursively write to the cache,
+       and the cache1 will hold the last cache1Size bytes
+
+       @param input the data to be written to the cache
+     **/
+    public void write(byte[] input){
+	
     }
 
     /**
