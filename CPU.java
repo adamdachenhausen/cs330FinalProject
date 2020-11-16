4b52965a249bfcbb031d0584355513daeced4b19
Adam Dachenhausen <ad10dach@siena.edu>
2020-11-15 19:37:30 -0500

    Added debug print line to help in CPU

--- CPU.java
+++ CPU.java
@@ -9,6 +9,9 @@ class CPU extends Thread{
     //The first cache in this
     private byte[] cache1;
 
+    //The index of the last element added
+    private int cache1Last;
+    
     //THe size of the first cache
     int cache1Size;
     /**
@@ -19,6 +22,7 @@ class CPU extends Thread{
     public CPU(int numCores, int cacheSize){
      cores = numCores;
      cache1Size = cacheSize;
+     cache1Last = -1; //We hold the index of the last item in the array
     }
     /**
        Reads the data in this' cache
@@ -27,17 +31,30 @@ class CPU extends Thread{
        in a real system will be much faster
      **/
     public byte[] read(){
-	return cache1
+	return cache1;
     }
     /**
        Writes input into the cache.
-       If input is bigger, then we will recursively write to the cache,
-       and the cache1 will hold the last cache1Size bytes
+       If input is bigger, then we will iteratively write to the cache,
+       and the cache1 will hold the last cache1Size bytes.
+
+       There will also not be any delay (other than overhead) as these are the
+       fastest memory locations.
 
        @param input the data to be written to the cache
+       @return 1 if the method is done, 0 if error occurs
      **/
-    public void write(byte[] input){
-	
+    public int write(byte[] input){
+	for(int i = 0; i<input.length;i++){
+	    add(input[i]);
+	}
+	return 1;
+    }
+    private void add(byte input){
+	cache1Last++;
+	System.out.println(cache1Last+" "+cache1Size);
+	if(cache1Last == cache1Size){cache1Last = 0;}
+	cache1[cache1Last] = input;
     }
 
     /**
