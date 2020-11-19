bbfaaabf2f4c382e8a0bd7d1d5b6a2b121fbfd25
Adam Dachenhausen <ad10dach@siena.edu>
2020-11-18 19:08:04 -0500

    Initial commit

---
+++ dataBlock.java
@@ -0,0 +1,17 @@
+/*
+  Represents a block of data on a HDD.
+  
+ */
+class dataBlock{
+    //The size of this block of data
+    int size;
+
+    
+    /**
+       Constructor for the dataBlock class
+       
+     **/
+    public dataBlock{
+	
+    }
+}
