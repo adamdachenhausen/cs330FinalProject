dd1cbad9173c0fa87958e32e09a3e51196d8cfaa
adamdachenhausen <ad10dach@siena.edu>
2020-11-19 13:18:38 -0500

    Fixed print crashing when data was empty

--- HDD.java
+++ HDD.java
@@ -1,6 +1,6 @@
 
 /*
-  This is a simulation of a Hard Drive Disk
+This is a simulation of a Hard Drive Disk
  */
 class HDD extends Thread{
     //The overall size of this
@@ -13,154 +13,161 @@ class HDD extends Thread{
     private dataBlock[] data;
 
     /**
-       Sets up a hard drive to be added to the system.
+    Sets up a hard drive to be added to the system.
 
-       @param size, the size in bytes of this
+    @param size, the size in bytes of this
      **/
     public HDD(int size){
-	this.size=size;
-	this.spaceLeft=size;
-	int totalBlocks;
-	if(size == 0){totalBlocks=0;}
-	else if(size<dataBlock.WIDTH){totalBlocks = 1;}
-	else{
-	    totalBlocks = size/dataBlock.WIDTH;
-	    if(size%dataBlock.WIDTH!=0){totalBlocks++;}
-	}
-	data = new dataBlock[totalBlocks];
+        this.size=size;
+        this.spaceLeft=size;
+        int totalBlocks;
+        if(size == 0){totalBlocks=0;}
+        else if(size<dataBlock.WIDTH){totalBlocks = 1;}
+        else{
+            totalBlocks = size/dataBlock.WIDTH;
+            if(size%dataBlock.WIDTH!=0){totalBlocks++;}
+        }
+        data = new dataBlock[totalBlocks];
     }
+
     /**
-       Writes a given set of bytes to this.
+    Writes a given set of bytes to this.
 
-       @param numBytes, the number of bytes to write to this
+    @param numBytes, the number of bytes to write to this
 
-       @return 1 if the write was successful, 0 if not (ie not enough space)
+    @return 1 if the write was successful, 0 if not (ie not enough space)
      **/
     public int write(int numBytes)
     {
-	//First let's check if we have enough raw space
-	if(numBytes > spaceLeft){return 0;}
+        //First let's check if we have enough raw space
+        if(numBytes > spaceLeft){return 0;}
 
-	//Now let's reserve our space and then write our data
-	spaceLeft =- numBytes;
+        //Now let's reserve our space and then write our data
+        spaceLeft =- numBytes;
 
-	//Now we call our helper method
-	int totalBlocks;
-	if(numBytes == 0){totalBlocks=0;}
-	else if(numBytes<dataBlock.WIDTH){totalBlocks = 1;}
-	else{
-	    totalBlocks = numBytes/dataBlock.WIDTH;
-	    if(numBytes%dataBlock.WIDTH!=0){totalBlocks++;}
-	}
-	return add(totalBlocks);
+        //Now we call our helper method
+        int totalBlocks;
+        if(numBytes == 0){totalBlocks=0;}
+        else if(numBytes<dataBlock.WIDTH){totalBlocks = 1;}
+        else{
+            totalBlocks = numBytes/dataBlock.WIDTH;
+            if(numBytes%dataBlock.WIDTH!=0){totalBlocks++;}
+        }
+        return add(totalBlocks);
     }
-    /**
-       FIFO add method. Finds a chunk of memory at least numBlocks big, and stores
-       numBlocks there.
 
-       @param numBlocks the number of blocks that need to be added to this
-       @return 1, if successful add, otherwise 0
+    /**
+    FIFO add method. Finds a chunk of memory at least numBlocks big, and stores
+    numBlocks there.
+
+    @param numBlocks the number of blocks that need to be added to this
+    @return 1, if successful add, otherwise 0
      **/
     private int add(int numBlocks){
-	int count = 0;
+        int count = 0;
 
-	//First we want to be able to loop through the entire data array
-	for(int i=0;i<data.length;i++){
-	    //Then we check if each one is open
-	    if(data[i] == null){
-		//Then we see if we have a hole big enough
-		while(count != numBlocks && data[count] == null ){
-		    count++;    
-		}
-		//Then we see if the hole is big enough
-		if(count == numBlocks){
-		    //Then we can add blocks starting at i, which has to be special
-		    data[i]=new dataBlock(true,numBlocks);
-		    count--;
-		    i++;
-		    while(count!=0){
-			data[i] = new dataBlock();
-			count--;
-			i++;
-		    }
-		    //Since we added the chunk, we can just return
-		    return 1;
-		}
-		//If we got here, that means we don't have a hole big enough, so let's reset count
-		//and skip those value by adding (count - 1) + i
-		count -= 1;
-		i += count;
-		count = 0;
-		
-	    }
-	}
-	return 0;
+        //First we want to be able to loop through the entire data array
+        for(int i=0;i<data.length;i++){
+            //Then we check if each one is open
+            if(data[i] == null){
+                //Then we see if we have a hole big enough
+                while(count != numBlocks && data[count] == null ){
+                    count++;    
+                }
+                //Then we see if the hole is big enough
+                if(count == numBlocks){
+                    //Then we can add blocks starting at i, which has to be special
+                    data[i]=new dataBlock(true,numBlocks);
+                    count--;
+                    i++;
+                    while(count!=0){
+                        data[i] = new dataBlock();
+                        count--;
+                        i++;
+                    }
+                    //Since we added the chunk, we can just return
+                    return 1;
+                }
+                //If we got here, that means we don't have a hole big enough, so let's reset count
+                //and skip those value by adding (count - 1) + i
+                count -= 1;
+                i += count;
+                count = 0;
 
-	
+            }
+        }
+        return 0;
+
     }
 
-	    
     /**
-       Reads data from this
-       Precondition: index is a dataBlock where .start is true.
-       @param index The index of the array where the data block starts
+    Reads data from this
+    Precondition: index is a dataBlock where .start is true.
+    @param index The index of the array where the data block starts
      **/
     public void read(int index){
 
-	//Loop through the data until we hit a null block or
-	//If we have two blocks back-to-back, then the start flag
-	//will be high, so we can stop reading our dataBlock
-	while(index<data.length && (data[index]!=null || data[index].start==true)){
-	    index++;   
-	    
-	}
+        //Loop through the data until we hit a null block or
+        //If we have two blocks back-to-back, then the start flag
+        //will be high, so we can stop reading our dataBlock
+        if(data!=null){
+            while(index<data.length && (data[index]!=null || data[index].start==true)){
+                index++;   
+
+            }
+        }
     }
+
     /**
-       Deletes data from this
-       Precondition: the index is a dataBlock where .start is true
-       @param index, the start index of the block of data to delete.
+    Deletes data from this
+    Precondition: the index is a dataBlock where .start is true
+    @param index, the start index of the block of data to delete.
      **/
     public void delete(int index){
-	//Loop through the data until we hit a null block or
-	//If we have two blocks back-to-back, then the start flag
-	//will be high, so we can stop reading our dataBlock
-	while(index<data.length && (data[index]!=null || data[index].start==true)){
-	    //Delete the current block
-	    data[index] = null;
+        //Loop through the data until we hit a null block or
+        //If we have two blocks back-to-back, then the start flag
+        //will be high, so we can stop reading our dataBlock
+        while(index<data.length && (data[index]!=null || data[index].start==true)){
+            //Delete the current block
+            data[index] = null;
 
-	    //Then increment and free space
-	    index++;
-	    spaceLeft+=dataBlock.WIDTH;
-	}
+            //Then increment and free space
+            index++;
+            spaceLeft+=dataBlock.WIDTH;
+        }
     }
+
     /**
-       Prints all the chunks(multiple dataBlocks) of data for the user to see what is on the disk.
-       Useful for selecting what data to read, write and delete.
+    Prints all the chunks(multiple dataBlocks) of data for the user to see what is on the disk.
+    Useful for selecting what data to read, write and delete.
      **/
     public void print(){
-	System.out.println("");
-	System.out.println("****Data on this hard drive****");
+        System.out.println("");
+        System.out.println("****Data on this hard drive****");
 
-	//First loop through the whole array
-	for(int i=0;i<data.length;i++){
-	    //Then if we encounter a dataBlock that is a start, print it
-	    if(data[i].start = true){
-		System.out.println(i+": "+data[i].size+" bytes big");
-	    }
-	}
-	System.out.println("*******************************");
+        //First loop through the whole array
+        if(data!=null){
+            for(int i=0;i<data.length;i++){
+                //Then if we encounter a dataBlock that is a start, print it
+                if(data[i]!=null && data[i].start == true){
+                    System.out.println(i+": "+data[i].size+" bytes big");
+                }
+            }
+        }
+        System.out.println("*******************************");
     }
+
     /**
-       
+
      **/
     @Override
     public void run(){
         //First we wait a bit to emulate the hard drive getting up to speed
-    
-    //Then we wait for instructions
-    while(!memSim.done){
-        //System.out.println("In HDD");
-        
-    }
+
+        //Then we wait for instructions
+        while(!memSim.done){
+            //System.out.println("In HDD");
+
+        }
     }
 }
--- RAM.java
+++ RAM.java
@@ -1,5 +1,5 @@
 /*
-  This is a simulation of Main System Memory aka Random Access Memory
+This is a simulation of Main System Memory aka Random Access Memory
  */
 class RAM extends Thread{
     //The size in bytes of this
@@ -7,125 +7,132 @@ class RAM extends Thread{
 
     //The spaceLeft in this
     int spaceLeft;
-    
+
     //The array to hold our data
     dataBlock[] data;
-    
+
     public RAM(int size){
         this.size = size;
-	this.spaceLeft = size;
-	
-	data = new dataBlock[size/dataBlock.WIDTH];
+        this.spaceLeft = size;
+
+        data = new dataBlock[size/dataBlock.WIDTH];
     }
+
     /**
-       Writes and input byte array to this
-       @param input the number of bytes to write
-       @return 1, if write is successful, 0 if not (ie not enough space left)
+    Writes and input byte array to this
+    @param input the number of bytes to write
+    @return 1, if write is successful, 0 if not (ie not enough space left)
      **/
     public int write(int input){
-	//First let's check that we have enough space
-	if(input > spaceLeft){return 0;}
+        //First let's check that we have enough space
+        if(input > spaceLeft){return 0;}
 
-	//Now let's reserve that space, and write our data
-	spaceLeft =- input;
+        //Now let's reserve that space, and write our data
+        spaceLeft =- input;
 
-	//Now we call our helper method
-	return add(input/dataBlock.WIDTH);
+        //Now we call our helper method
+        return add(input/dataBlock.WIDTH);
 
-	
     }
+
     private int add(int numBlocks){
-	int count = 0;
+        int count = 0;
 
-	//First we want to be able to loop through the entire data array
-	for(int i=0;i<data.length;i++){
-	    //Then we check if each one is open
-	    if(data[i] == null){
-		count = 1;
-		//Then we see if we have a hole big enough
-		while(data[count] == null && count != numBlocks){
-		    count++;
-		}
-		//Then we see if the hole is big enough
-		if(count == numBlocks){
-		    //Then we can add blocks starting at i, which has to be special
-		    data[i] = new dataBlock(true,numBlocks);
-		    count--;
-		    i++;
-		    while(count!=0){
-			data[i] = new dataBlock();
-			count--;
-			i++;
-		    }
-		    //Since we aded the chunk, we can just return
-		    return 1;
-		}
-		//If we got here, that means we don't have a hole big enough, so let's reset count
-		//and skip those values by adding (count - 1) and i
-		count -= 1;
-		i += count;
-		count = 0;
-	    }
-	}
-	return 0;
+        //First we want to be able to loop through the entire data array
+        for(int i=0;i<data.length;i++){
+            //Then we check if each one is open
+            if(data[i] == null){
+                count = 1;
+                //Then we see if we have a hole big enough
+                while(data[count] == null && count != numBlocks){
+                    count++;
+                }
+                //Then we see if the hole is big enough
+                if(count == numBlocks){
+                    //Then we can add blocks starting at i, which has to be special
+                    data[i] = new dataBlock(true,numBlocks);
+                    count--;
+                    i++;
+                    while(count!=0){
+                        data[i] = new dataBlock();
+                        count--;
+                        i++;
+                    }
+                    //Since we aded the chunk, we can just return
+                    return 1;
+                }
+                //If we got here, that means we don't have a hole big enough, so let's reset count
+                //and skip those values by adding (count - 1) and i
+                count -= 1;
+                i += count;
+                count = 0;
+            }
+        }
+        return 0;
     }
+
     /**
-       Reads data from this, and returns it
-       Precondition the index is a datablock with .start = true
-       @param the index of the starting array index
+    Reads data from this, and returns it
+    Precondition the index is a datablock with .start = true
+    @param the index of the starting array index
      **/
     public void read(int index){
-	//Loop through the data until we hit a null block or
-	//If we have two blocks back-to-back, then the start flag
-	//will be high, so we can stop reading our dataBlock
-	while(data[index]!=null || data[index].start==true){
-	    index++;
-	}
+        //Loop through the data until we hit a null block or
+        //If we have two blocks back-to-back, then the start flag
+        //will be high, so we can stop reading our dataBlock
+        while(data[index]!=null || data[index].start==true){
+            index++;
+        }
     }
+
     /**
-       Deletes data from this
-       Precondition the index is a datablock with .start = true
-       @param the index of the starting point to delete from
+    Deletes data from this
+    Precondition the index is a datablock with .start = true
+    @param the index of the starting point to delete from
      **/
     public void delete(int index){
-	//Loop through the data until we hit a null block
-	//If we have two blocks back-to-back, then the start flag
-	//will be high, so we can stop reading our dataBlock
-	if(data!=null){
-	while(data[index]!=null || data[index].start==true){
-	    //Delete the current block
-	    data[index]=null;
+        //Loop through the data until we hit a null block
+        //If we have two blocks back-to-back, then the start flag
+        //will be high, so we can stop reading our dataBlock
+        if(data!=null){
+            while(data[index]!=null || data[index].start==true){
+                //Delete the current block
+                data[index]=null;
 
-	    //Then increment and free space
-	    index++;
-	    spaceLeft+=dataBlock.WIDTH;
-	}
-	}
+                //Then increment and free space
+                index++;
+                spaceLeft+=dataBlock.WIDTH;
+            }
+        }
     }
-   /**
-       Prints all the chunks(multiple dataBlocks) of data for the user to see what is on the disk.
-       Useful for selecting what data to read, write and delete.
+
+    /**
+    Prints all the chunks(multiple dataBlocks) of data for the user to see what is on the disk.
+    Useful for selecting what data to read, write and delete.
      **/
     public void print(){
-	System.out.println("");
-	System.out.println("****Data on this RAM Stick****");
+        System.out.println("");
+        System.out.println("****Data on this RAM Stick****");
 
-	//First loop through the whole array
-	for(int i=0;i<data.length;i++){
-	    //Then if we encounter a dataBlock that is a start, print it
-	    if(data[i].start = true){
-		System.out.println(i+": "+data[i].size+" bytes big");
-	    }
-	}
-	System.out.println("*******************************");
+        //First loop through the whole array
+        if(data!=null){
+            for(int i=0;i<data.length;i++){
+                //Then if we encounter a dataBlock that is a start, print it
+                if(data[i]!=null && data[i].start == true){
+                    System.out.println(i+": "+data[i].size+" bytes big");
+                }
+            }
+        }
+        System.out.println("*******************************");
     }
+
     /**
-       
+
      **/
     @Override
     public void run(){
         while(!memSim.done){
             //System.out.print("In Ram");
-    }
+        }
     }
 }
